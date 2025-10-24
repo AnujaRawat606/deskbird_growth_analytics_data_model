38bed306dc25f6b8817508805852cff49ed782c9
Anuja Rawat <anujarawat606@gmail.com>
2025-10-24 22:26:44 +0200

    Refactor product engagement funnel SQL query

--- dbt_models/marts/fact_table/product_engagement_funnel.sql
+++ dbt_models/marts/fact_table/product_engagement_funnel.sql
@@ -3,8 +3,9 @@
       when month(created_at) < 10 then concat(year(created_at), "-M0", month(created_at))
       else concat(year(created_at), "-M", month(created_at))
     end as lead_created_month,
-    campaign_id,
-    utm_campaign,
+    a.campaign_id,
+    channel,
+    a.utm_campaign,
     lead_id,
     a.company_id,
     company_name,
@@ -15,14 +16,10 @@
     end as usage_month,
     event_type,
     plan_tier,
-    count(user_id) as total_users,
-    sum(seats_used) as total_seats_used
+    user_id,
+    seats_used
 from default.crm_pipeline a  
 left join default.product_usage_events b 
   on a.company_id = b.company_id
-group by all
-order by a.company_id, 
-    case 
-      when month(event_time) < 10 then concat(year(event_time), "-M0", month(event_time))
-      else concat(year(event_time), "-M", month(event_time))
-    end
+left join default.paid_ads_deskbird c  
+  on a.campaign_id = c.campaign_id
